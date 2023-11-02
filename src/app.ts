0484f9482cd6422613ac62cad761b0657ff0d3d9
Nick Hodges <nickhodges@gmail.com>
2023-11-02 19:48:54 -0400

    Updated greeting

--- src/app.ts
+++ src/app.ts
@@ -26,7 +26,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Hello, Aptible User!');
 });
 
 // Endpoints
