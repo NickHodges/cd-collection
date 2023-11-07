b0ceb10560c11b543ea641affaecb259001da8e9
Nick Hodges <nickhodges@gmail.com>
2023-11-06 20:17:06 -0500

    Back to 'Hello World'

--- src/app.ts
+++ src/app.ts
@@ -26,7 +26,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, Aptible User!');
+  res.send('Hello, World!');
 });
 
 // Endpoints
